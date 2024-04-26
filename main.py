0ef65c846809e8263add82820391354a704602b8
vanparfait <nguemechiaparfait@gmail.com>
2024-04-26 15:58:58 +0100

    script pour lire le contenu de notre fichier

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 # Écrivez votre code ici !
+import csv
+
+#en_tete = ["nom" , "heures_travaillees"]
+
+def extract(nom_du_fichier):
+    with open(nom_du_fichier,"r") as fichier_csv:
+        reader = csv.DictReader(fichier_csv, delimiter=",")
+        for ligne in reader:
+            print(ligne)
+        
+extract("input.csv")
 
 
 # Ne touchez pas le code ci-dessous
-if __name__ == "__main__":
-    main()
+if __name__ == "__min__":
+    min()
